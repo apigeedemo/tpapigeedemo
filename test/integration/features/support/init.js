b5f3ca86aafb74feab6d1cb4982b12ec3ce3fa66
Harish Kumar <harishisin@gmail.com>
2022-10-22 13:35:54 +0100

    Removed test steps

--- test/integration/features/support/init.js
+++ test/integration/features/support/init.js
@@ -7,7 +7,7 @@ const {defineSupportCode} = require('cucumber');
 defineSupportCode(function({Before}) {
     Before(function() {
     // ******** ADD HERE APIGEE PROXY ENDPOINT HOSTNAME **********
-        this.apickli = new apickli.Apickli('https', 'organization_hostname/airports-cicd/v1');
+        this.apickli = new apickli.Apickli('https', 'organization_hostname/hello-world');
         this.apickli.addRequestHeader('Cache-Control', 'no-cache');
     });
 });
